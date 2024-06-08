f8933d25a929f551de4caad66ff7c33dc38cc970
rowpacmal <rowpacmal@outlook.com>
2024-06-08 23:30:47 +0200

    Added error handler & wrong resource catcher

--- server/server.mjs
+++ server/server.mjs
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import resourceNotFound from './utils/resourceNotFound.mjs';
+import errorHandler from './middlewares/errorHandler.mjs';
+
 dotenv.config({ path: 'config/config.env' });
 
 const app = express();
 app.use(express.json());
+
+app.all('*', resourceNotFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
