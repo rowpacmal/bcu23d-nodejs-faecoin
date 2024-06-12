6ba452c928bfd49fe35b24f2902d315e419ffef0
rowpacmal <rowpacmal@outlook.com>
2024-06-12 15:53:09 +0200

    Added updateChain and validateChain to the blockchain class

--- server/models/Blockchain.mjs
+++ server/models/Blockchain.mjs
@@ -1,3 +1,4 @@
+import { createHash } from '../utils/cryptoLib.mjs';
 import Block from './Block.mjs';
 
 export default class Blockchain {
@@ -11,5 +12,37 @@ export default class Blockchain {
     this.chain.push(block);
 
     return block;
+  }
+
+  updateChain(chain) {
+    if (chain.length <= this.chain.length) return;
+    if (!Blockchain.validateChain(chain)) return;
+
+    this.chain = chain;
+  }
+
+  static validateChain(chain) {
+    if (JSON.stringify(chain.at(0)) !== JSON.stringify(Block.genesis))
+      return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const { index, timestamp, prevHash, hash, data, nonce, difficulty } =
+        chain.at(i);
+      const { hash: lastHash, difficulty: lastDifficulty } = chain.at(i - 1);
+      const validHash = createHash(
+        index,
+        timestamp,
+        prevHash,
+        data,
+        nonce,
+        difficulty
+      );
+
+      if (hash !== validHash) return false;
+      if (prevHash !== lastHash) return false;
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
+
+      return true;
+    }
   }
 }
