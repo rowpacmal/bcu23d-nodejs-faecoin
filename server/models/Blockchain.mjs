6c1f2ad049e7a032e51bf1983e7acb89ae2f75bf
rowpacmal <rowpacmal@outlook.com>
2024-06-15 15:47:43 +0200

    Changed blockchain model into a mongoose schema

--- server/models/Blockchain.mjs
+++ server/models/Blockchain.mjs
@@ -1,48 +1,50 @@
+import mongoose from 'mongoose';
+
 import { createHash } from '../utils/cryptoLib.mjs';
 import Block from './Block.mjs';
 
-export default class Blockchain {
-  constructor() {
-    this.chain = [Block.genesis];
+const blockchainSchema = mongoose.Schema({
+  chain: { type: [Object], required: true, default: [Block.genesis] },
+});
+
+blockchainSchema.methods.addBlock = function ({ data }) {
+  const block = Block.mine({ prevBlock: this.chain.at(-1), data });
+
+  this.chain.push(block);
+
+  return block;
+};
+
+blockchainSchema.methods.updateChain = function (chain) {
+  if (chain.length <= this.chain.length) return;
+  if (!this.constructor.validateChain(chain)) return;
+
+  this.chain = chain;
+};
+
+blockchainSchema.statics.validateChain = function (chain) {
+  if (JSON.stringify(chain.at(0)) !== JSON.stringify(Block.genesis))
+    return false;
+
+  for (let i = 1; i < chain.length; i++) {
+    const { index, timestamp, prevHash, hash, data, nonce, difficulty } =
+      chain.at(i);
+    const { hash: lastHash, difficulty: lastDifficulty } = chain.at(i - 1);
+    const validHash = createHash(
+      index,
+      timestamp,
+      prevHash,
+      data,
+      nonce,
+      difficulty
+    );
+
+    if (hash !== validHash) return false;
+    if (prevHash !== lastHash) return false;
+    if (Math.abs(lastDifficulty - difficulty) > 1) return false;
   }
 
-  addBlock(data) {
-    const block = Block.mine({ prevBlock: this.chain.at(-1), data });
+  return true;
+};
 
-    this.chain.push(block);
-
-    return block;
-  }
-
-  updateChain(chain) {
-    if (chain.length <= this.chain.length) return;
-    if (!Blockchain.validateChain(chain)) return;
-
-    this.chain = chain;
-  }
-
-  static validateChain(chain) {
-    if (JSON.stringify(chain.at(0)) !== JSON.stringify(Block.genesis))
-      return false;
-
-    for (let i = 1; i < chain.length; i++) {
-      const { index, timestamp, prevHash, hash, data, nonce, difficulty } =
-        chain.at(i);
-      const { hash: lastHash, difficulty: lastDifficulty } = chain.at(i - 1);
-      const validHash = createHash(
-        index,
-        timestamp,
-        prevHash,
-        data,
-        nonce,
-        difficulty
-      );
-
-      if (hash !== validHash) return false;
-      if (prevHash !== lastHash) return false;
-      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
-    }
-
-    return true;
-  }
-}
+export default mongoose.model('Blockchain', blockchainSchema);
--- server/startup.mjs
+++ server/startup.mjs
@@ -7,17 +7,24 @@ import Blockchain from './models/Blockchain.mjs';
 import TransactionPool from './models/TransactionPool.mjs';
 import Wallet from './models/Wallet.mjs';
 import PubNubServer from './models/PubNubServer.mjs';
+import User from './models/User.mjs';
 
 export let blockchain, transactionPool, wallet, pubnub;
 
+const createConstructor = async (prototype) => {
+  if (!(await prototype.find())[0]) {
+    return await prototype.create({});
+  } else {
+    return new prototype((await prototype.find())[0]);
+  }
+};
+
 export const startup = async () => {
   dotenv.config({ path: 'config/config.env' });
+  global.__rootdirname = path.dirname(fileURLToPath(import.meta.url));
 
   const mongo = await mongoose.connect(process.env.MONGODB_URI);
-
   console.log(`MongoDB connected to ${mongo.connection.host}`);
-
-  global.__rootdirname = path.dirname(fileURLToPath(import.meta.url));
 
   const credentials = {
     publishKey: process.env.PUBNUB_PUB_KEY,
@@ -26,7 +33,7 @@ export const startup = async () => {
     userId: process.env.PUBNUB_USER_ID,
   };
 
-  blockchain = new Blockchain();
+  blockchain = await createConstructor(Blockchain);
   transactionPool = new TransactionPool();
   wallet = new Wallet();
   pubnub = new PubNubServer({
@@ -35,4 +42,10 @@ export const startup = async () => {
     wallet,
     credentials,
   });
+
+  // await User.create({
+  //   name: 'Test',
+  //   email: 'info@test.me',
+  //   password: 'test1!',
+  // });
 };
