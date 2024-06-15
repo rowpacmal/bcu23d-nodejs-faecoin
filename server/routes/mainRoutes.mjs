9e14d90b617d75f080c6a20fd4ff8f42e3ff6724
rowpacmal <rowpacmal@outlook.com>
2024-06-15 21:01:33 +0200

    Updated main router with new admin and auth routes

--- server/routes/mainRoutes.mjs
+++ server/routes/mainRoutes.mjs
@@ -2,10 +2,13 @@ import express from 'express';
 
 import blockchainRouter from './blockchainRoutes.mjs';
 import transactionRouter from './transactionRoutes.mjs';
+import userRouter from './userRoutes.mjs';
 
 const mainRouter = express.Router();
 
 mainRouter.use('/blockchain', blockchainRouter);
 mainRouter.use('/transactions', transactionRouter);
+mainRouter.use('/auth', (req, res, next) => {});
+mainRouter.use('/admin/users', userRouter);
 
 export default mainRouter;
