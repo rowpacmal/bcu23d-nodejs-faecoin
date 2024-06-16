1da55ab0dbe77393beb117e90976c067fc8957b5
rowpacmal <rowpacmal@outlook.com>
2024-06-16 16:30:10 +0200

    Created the router for user created accounts

---
+++ server/routes/authRoutes.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+  getMyAccount,
+  loginToAccount,
+  registerAccount,
+  updateAccountDetails,
+  updateAccountPassword,
+} from '../controllers/authController.mjs';
+import {
+  authorizedAccess,
+  protectedAccess,
+} from '../middlewares/authorization.mjs';
+
+const authRouter = express.Router();
+
+authRouter.route('/register').post(registerAccount);
+authRouter.route('/login').post(loginToAccount);
+
+authRouter.use(protectedAccess);
+authRouter.route('/me').get(getMyAccount);
+authRouter.route('/edit/details').put(updateAccountDetails);
+authRouter.route('/edit/password').put(updateAccountPassword);
+
+export default authRouter;
