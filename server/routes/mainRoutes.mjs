db3dcc01baa86b89425218abd0564537445f874a
rowpacmal <rowpacmal@outlook.com>
2024-06-16 15:12:44 +0200

    Updated main router structure

--- server/routes/mainRoutes.mjs
+++ server/routes/mainRoutes.mjs
@@ -1,14 +1,15 @@
 import express from 'express';
 
+import authRouter from './authRoutes.mjs';
 import blockchainRouter from './blockchainRoutes.mjs';
 import transactionRouter from './transactionRoutes.mjs';
 import userRouter from './userRoutes.mjs';
 
 const mainRouter = express.Router();
 
+mainRouter.use('/admin/users', userRouter);
+mainRouter.use('/auth', authRouter);
 mainRouter.use('/blockchain', blockchainRouter);
 mainRouter.use('/transactions', transactionRouter);
-mainRouter.use('/auth', (req, res, next) => {});
-mainRouter.use('/admin/users', userRouter);
 
 export default mainRouter;
