0a6469b6d8860fc99373967282c5b091e7d8e81d
rowpacmal <rowpacmal@outlook.com>
2024-06-16 16:28:52 +0200

    Created auth router for users created accounts

---
+++ server/controllers/authController.mjs
@@ -0,0 +1,72 @@
+import asyncHandler from '../middlewares/asyncHandler.mjs';
+import User from '../models/User.mjs';
+import DataResponse from '../models/DataResponse.mjs';
+import ErrorResponse from '../models/ErrorResponse.mjs';
+
+export const registerAccount = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  generateAndSendToken(user, res, 'Successfully created an account', 201);
+});
+
+export const loginToAccount = asyncHandler(async (req, res, next) => {
+  const errorRes = new ErrorResponse('Invalid login', 401);
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new ErrorResponse('Email and/or password is missing', 400));
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) return next(errorRes);
+
+  const isValid = await user.validatePassword(password);
+  if (!isValid) return next(errorRes);
+
+  generateAndSendToken(user, res, 'Successfully signed in', 201);
+});
+
+export const getMyAccount = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json(
+    new DataResponse({
+      message: `Welcome ${req.user.name} to your account!`,
+      statusCode: 200,
+      data: user,
+    })
+  );
+});
+
+export const updateAccountDetails = asyncHandler(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  await User.findByIdAndUpdate(
+    req.user.id,
+    { name, email },
+    {
+      runValidators: true,
+    }
+  );
+
+  res.status(204).send();
+});
+
+export const updateAccountPassword = asyncHandler(async (req, res, next) => {
+  const { currentPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.validatePassword(currentPassword)))
+    return next(new ErrorResponse('Invalid password', 401));
+
+  user.password = newPassword;
+  await user.save();
+
+  generateAndSendToken(user, res, 'Successfully updated the password', 204);
+});
+
+const generateAndSendToken = (user, res, message, statusCode) => {
+  const token = user.createToken();
+
+  res.status(201).json(new DataResponse({ message, statusCode, data: token }));
+};
