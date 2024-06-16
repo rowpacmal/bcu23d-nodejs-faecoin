d4ee92a0fd63f5968bd2ce8de144d4cf1664bc44
rowpacmal <rowpacmal@outlook.com>
2024-06-16 16:27:19 +0200

    Updated error handler; updated package.json for dev

--- server/middlewares/errorHandler.mjs
+++ server/middlewares/errorHandler.mjs
@@ -1,13 +1,30 @@
 import DataResponse from '../models/DataResponse.mjs';
+import ErrorResponse from '../models/ErrorResponse.mjs';
 import FileHandler from '../models/FileHandler.mjs';
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  new FileHandler('logs', 'error.log').append(`${err.message}\n\n`);
+  let error = { ...err, message: err.message };
+
+  if (error.code === 11000)
+    error = new ErrorResponse('Resource already exists', 400);
+
+  if (err.name === 'JsonWebTokenError')
+    error = new ErrorResponse('Invalid token, access denied', 401);
+
+  if (err.name === 'ValidationError') {
+    const validation = Object.values(err.errors)
+      .map((value) => value.message)
+      .join(', ');
+
+    error = new ErrorResponse(`Invalid request, ${validation}`, 400);
+  }
+
+  const statusCode = error.statusCode || 500;
+  new FileHandler('logs', 'error.log').append(`${error.message}\n\n`);
 
   res
-    .status(err.statusCode || 500)
-    .json(new DataResponse({ error: err.message, statusCode }));
+    .status(statusCode)
+    .json(new DataResponse({ error: error.message, statusCode }));
 };
 
 export default errorHandler;
