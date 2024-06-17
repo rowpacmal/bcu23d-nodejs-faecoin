d3d1965f094feaa96b1c02dcfb8967b3550b9ae8
rowpacmal <rowpacmal@outlook.com>
2024-06-17 10:53:33 +0200

    Added additional routes to the blockchain router

--- server/controllers/blockchainController.mjs
+++ server/controllers/blockchainController.mjs
@@ -4,15 +4,49 @@ import ErrorResponse from '../models/ErrorResponse.mjs';
 
 export const getAllBlocks = (req, res, next) => {
   try {
-    res
-      .status(200)
-      .json(
-        new DataResponse({
-          message: 'Successfully fetched the chain',
-          statusCode: 200,
-          data: blockchain.chain,
-        })
-      );
+    res.status(200).json(
+      new DataResponse({
+        message: 'Successfully fetched the chain',
+        statusCode: 200,
+        data: blockchain.chain,
+      })
+    );
+  } catch (error) {
+    next(new ErrorResponse(error.message, 500));
+  }
+};
+
+export const getBlockByIndex = (req, res, next) => {
+  const { index } = req.params;
+  const block = blockchain.chain.find((block) => block.index === +index);
+
+  if (!block)
+    return next(
+      new ErrorResponse(`Unable to find a block with index: ${index}`, 404)
+    );
+
+  try {
+    res.status(200).json(
+      new DataResponse({
+        message: 'Successfully fetched a block',
+        statusCode: 200,
+        data: block,
+      })
+    );
+  } catch (error) {
+    next(new ErrorResponse(error.message, 500));
+  }
+};
+
+export const getLatestBlock = (req, res, next) => {
+  try {
+    res.status(200).json(
+      new DataResponse({
+        message: 'Successfully fetched the latest block',
+        statusCode: 200,
+        data: blockchain.chain.at(-1),
+      })
+    );
   } catch (error) {
     next(new ErrorResponse(error.message, 500));
   }
--- server/routes/blockchainRoutes.mjs
+++ server/routes/blockchainRoutes.mjs
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getAllBlocks } from '../controllers/blockchainController.mjs';
+import {
+  getAllBlocks,
+  getBlockByIndex,
+  getLatestBlock,
+} from '../controllers/blockchainController.mjs';
 
 const blockchainRouter = express.Router();
 
 blockchainRouter.route('/').get(getAllBlocks);
+blockchainRouter.route('/blocks/latest').get(getLatestBlock);
+blockchainRouter.route('/blocks/:index').get(getBlockByIndex);
 
 export default blockchainRouter;
