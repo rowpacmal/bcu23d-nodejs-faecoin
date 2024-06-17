6c4f4e9ce852a284985a12bca198e4e4be86599b
rowpacmal <rowpacmal@outlook.com>
2024-06-17 10:53:15 +0200

    Added authentication to transaction routes

--- server/routes/transactionRoutes.mjs
+++ server/routes/transactionRoutes.mjs
@@ -5,13 +5,21 @@ import {
   getWalletBalance,
   mineTransactions,
 } from '../controllers/transactionController.mjs';
+import {
+  authorizedAccess,
+  protectedAccess,
+} from '../middlewares/authorization.mjs';
 
 const transactionRouter = express.Router();
 
-transactionRouter.route('/').get(getTransactionPool).post(addTransaction);
+transactionRouter.use(protectedAccess);
 
+transactionRouter.route('/').get(getTransactionPool);
 transactionRouter.route('/wallet').get(getWalletBalance);
 
+transactionRouter.use(authorizedAccess('manager', 'admin'));
+
+transactionRouter.route('/add').post(addTransaction);
 transactionRouter.route('/mine').get(mineTransactions);
 
 export default transactionRouter;
