f03811cbdde50503b0cb767c2ac4b12668ee9d17
rowpacmal <rowpacmal@outlook.com>
2024-07-01 14:21:18 +0200

    Updated the home page component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Hero from '../components/home/Hero';
+import Introduction from '../components/home/Introduction';
+import Newsletter from '../components/home/Newsletter';
+
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <Hero />
+
+      <Newsletter />
+
+      <Introduction />
+    </>
+  );
 }
 
 export default Home;
