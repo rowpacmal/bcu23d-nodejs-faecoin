f15fdfdeb398c565f4e1bf022e0b4dfe49d40227
rowpacmal <rowpacmal@outlook.com>
2024-07-06 10:30:09 +0200

    Removed unused routes and controller

--- server/routes/mainRoutes.mjs
+++ server/routes/mainRoutes.mjs
@@ -3,12 +3,10 @@ import express from 'express';
 import authRouter from './authRoutes.mjs';
 import blockchainRouter from './blockchainRoutes.mjs';
 import transactionRouter from './transactionRoutes.mjs';
-import userRouter from './userRoutes.mjs';
 import walletRouter from './walletRoutes.mjs';
 
 const mainRouter = express.Router();
 
-mainRouter.use('/admin/users', userRouter);
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/blockchain', blockchainRouter);
 mainRouter.use('/transactions', transactionRouter);
